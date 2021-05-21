98a52a48ae3f0530b8a43ab8a929a709d00a0185
Dat <haruthong@gmail.com>
2021-05-20 19:32:38 -0700

    completed

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("dfafd")
+// Scrolling Fade-in Slide up effect
+window.addEventListener("scroll", () => {
+	// Loop through all scrolling classes
+	document.querySelectorAll(".scrolling-effect").forEach((scroll) => {
+		// top viewport base on user scrolling
+		const top = scroll.getBoundingClientRect().top;
+		// Compare to window height , add effect if it smaller then window height
+		if (top < window.innerHeight) {
+			scroll.classList.add("slide-up-effect");
+			scroll.classList.remove("opacity");
+		}
+	});
+});
